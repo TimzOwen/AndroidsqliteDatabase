fe0e6d26f4e5d1587c2125fd7e7c9089510b59d5
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-22 13:07:55 +0300

    updated strings and UI

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
@@ -4,14 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 public class CatalogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
 
         //floating action button
 //        FloatingActionButton fab =
