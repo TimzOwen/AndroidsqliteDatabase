830193477396326901611519fd884ba7d8f91240
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-22 13:52:36 +0300

    updated icon

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
