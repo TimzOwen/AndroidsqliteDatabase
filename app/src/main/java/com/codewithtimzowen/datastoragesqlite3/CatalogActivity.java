0c00f84c11e6e946aef43de794f78412b4f47c1c
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-22 13:31:49 +0300

    updated the master catalog

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
@@ -2,7 +2,13 @@ package com.codewithtimzowen.datastoragesqlite3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -11,7 +17,37 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-        //floating action button
-//        FloatingActionButton fab =
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab =  findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+    //create menus inflation
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                // Do nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/EditorActivity.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/EditorActivity.java
@@ -3,8 +3,29 @@ package com.codewithtimzowen.datastoragesqlite3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 
 public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the pet's name */
+    private EditText mNameEditText;
+
+    /** EditText field to enter the pet's breed */
+    private EditText mBreedEditText;
+
+    /** EditText field to enter the pet's weight */
+    private EditText mWeightEditText;
+
+    /** EditText field to enter the pet's gender */
+    private Spinner mGenderSpinner;
+
+    /**
+     * Gender of the pet. The possible values are:
+     * 0 for unknown gender, 1 for male, 2 for female.
+     */
+    private int mGender = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
