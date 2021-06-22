6b2909a9513d96e35710a733036c5b302ca47f1e
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-22 12:46:42 +0300

    updated UI for editor

---
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/CatalogActivity.java
@@ -0,0 +1,19 @@
+package com.codewithtimzowen.datastoragesqlite3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //floating action button
+//        FloatingActionButton fab =
+    }
+}
