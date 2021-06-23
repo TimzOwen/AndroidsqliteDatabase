37770821a43ff11dc1e4a6760f188ad4d828fdb6
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-23 19:06:35 +0300

    switched to gender on data

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
@@ -20,6 +20,11 @@ public final class PetContract {
         public final static String COLUMN_PET_WEIGHT = "weight";
 
 
+        //Gender
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
 
 
     }
