4d57811695cfaa274f2457fb3c71bf9fbd05f689
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-23 09:28:31 +0300

    updated column final strings

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
@@ -13,6 +13,14 @@ public final class PetContract {
 
         public final static String TABLE_NAME = "pets";
 
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME = "name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENDER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+
+
 
     }
 }
