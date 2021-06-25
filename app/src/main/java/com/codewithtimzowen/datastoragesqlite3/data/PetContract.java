330a42629983a2e9470830688fb9a348071c4b6c
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-25 14:35:41 +0300

    updated SQLite helper

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
@@ -20,7 +20,7 @@ public final class PetContract {
         public final static String COLUMN_PET_WEIGHT = "weight";
 
 
-        //Gender
+        //Gender for th e menu switch statement
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
