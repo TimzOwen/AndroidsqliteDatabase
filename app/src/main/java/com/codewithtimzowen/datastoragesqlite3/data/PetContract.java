8ac72ad36d215201b3a2f43d8bddf1fa5f13ea12
TimzOwen <ktimothy@kabarak.ac.ke>
2021-06-26 15:28:46 +0300

    updated conten values and Uti matcher

--- app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
+++ app/src/main/java/com/codewithtimzowen/datastoragesqlite3/data/PetContract.java
@@ -4,6 +4,9 @@ import android.provider.BaseColumns;
 
 public final class PetContract {
 
+    public static final String CONTENT_AUTHORITY = "com.codewithtimzowen.datastoragesqlite3.data/pets";
+    public static final String PATH_PETS = "uri_matcher_object";
+
     //private constructor
     private PetContract(){
 
